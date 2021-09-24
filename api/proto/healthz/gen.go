06cb956542a3b23890e094a251194252f35cb66e
Folke Behrens <folke@gmail.com>
2021-09-24 22:07:37 +0200

    Use 'buf' to generate proto code.

--- api/proto/healthz/gen.go
+++
@@ -1,6 +0,0 @@
-//go:generate protoc -I. -I../../../vendor/github.com/googleapis/googleapis/ --go_out . --go_opt paths=source_relative healthz.proto
-//go:generate protoc -I. -I../../../vendor/github.com/googleapis/googleapis/ --go-grpc_out . --go-grpc_opt paths=source_relative --go-grpc_opt=require_unimplemented_servers=false healthz.proto
-//go:generate protoc -I. -I../../../vendor/github.com/googleapis/googleapis/ --grpc-gateway_out . --grpc-gateway_opt paths=source_relative healthz.proto
-//go:generate protoc -I. -I../../../vendor/github.com/googleapis/googleapis/ --openapiv2_out . healthz.proto
-
-package healthz
